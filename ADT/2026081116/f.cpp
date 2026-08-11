7d1cd58a7cf4ba9685d9d0043d8cd8156a51eb2b
N-Nanmo <takoyaki450en@gmail.com>
2026-08-11 17:44:32 +0900

    精進+ADT2026081116:00

---
+++ ADT/2026081116/f.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vb = vector<bool>;
+using vc = vector<char>;
+using vf = vector<float>;
+using vd = vector<double>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvs = vector<vector<string>>;
+using vvc = vector<vector<char>>;
+using vvb = vector<vector<bool>>;
+using vvf = vector<vector<float>>;
+using vvd = vector<vector<double>>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpi = vector<pi>;
+using vpll = vector<pll>;
+#define cYes cout << "YES\n"
+#define cNo cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+template <typename T>
+inline istream& operator >> (istream& is, vector<T>& v){
+    for(size_t i=0; i<v.size(); i++){
+        is >> v[i];
+    }
+    return is;
+}
+template <typename T, typename U>
+inline istream& operator >> (istream& is, vector<pair<T,U>>& v){
+    for(size_t i=0; i<v.size(); i++){
+        is >> v[i].first >> v[i].second;
+    }
+    return is;
+}
+template <typename T>
+inline istream& operator >> (istream& is, vector<vector<T>>& v){
+    for(size_t i=0; i<v.size(); i++){
+        for(size_t j=0; j<v[i].size(); j++){
+            is >> v[i][j];
+        }
+    }
+    return is;
+}
+template <typename T, typename U, typename S, typename V>
+inline istream& operator >> (istream& is, vector<pair<pair<T,U>, pair<S,V>>>& v){
+    for(size_t i=0; i<v.size(); i++){
+        is >> v[i].first.first >> v[i].first.second >> v[i].second.first >> v[i].second.second;
+    }
+    return is;
+}
+const double PI = 3.14159265359;
+
+ll dfs(int N, int K, vll &A, vll crr){
+    if(crr.size() == K){
+        ll exp = 0;
+        for(int i=0; i<K; i++){
+            exp += A[crr[i]];
+        }
+        string S = to_string(exp);
+        ll ans = 0;
+        for(int i=0; i<S.size(); i++){
+            ans += (S[i]-'0')%5;
+            ans += (S[i]-'0')/5;
+        }
+        return ans;
+    }else if(crr.size() != 0 && crr[crr.size()-1] == N-1){
+        return LLONG_MAX;
+    }
+    int i = 0;
+    ll ans = LLONG_MAX;
+    if(crr.size() != 0) i = crr[crr.size()-1]+1;
+    for(; i<N; i++){
+        crr.emplace_back(i);
+        ans = min(ans, dfs(N, K, A, crr));
+        crr.pop_back();
+    }
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, K;
+    cin >> N >> K;
+    vi A(N);
+    cin >> A;
+    sort(all(A));
+    vll sm;
+    int last = A[0];
+    ll crr = A[0];
+    for(int i=1; i<N; i++){
+        if(last != A[i]){
+            sm.emplace_back(crr);
+            crr = 0;
+        }
+        last = A[i];
+        crr += A[i];
+    }
+    if(crr != 0) sm.emplace_back(crr);
+    sort(rall(sm));
+    ll ans = 0;
+    for(int i=K; i<sm.size(); i++){
+        ans += sm[i];
+    }
+    cout << ans << "\n";
+}
